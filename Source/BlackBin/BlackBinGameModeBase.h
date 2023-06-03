038d6b5b78612397681efc4666cd439089a46dc9
obdo <81908835+fortress43-dev@users.noreply.github.com>
2023-06-03 09:45:52 +0900

    Init Project

---
+++ Source/BlackBin/BlackBinGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BlackBinGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLACKBIN_API ABlackBinGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
