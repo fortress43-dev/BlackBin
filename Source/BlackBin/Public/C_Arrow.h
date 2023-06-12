2b6ea362ede0a73ace85c9b129f0cd9a21295c03
githeed <tmdgmldhk11@gmail.com>
2023-06-12 10:16:43 +0900

    11

---
+++ Source/BlackBin/Public/C_Arrow.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "C_HitBox.h"
+#include "C_Arrow.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLACKBIN_API AC_Arrow : public AC_HitBox
+{
+	GENERATED_BODY()
+
+protected:
+	// Called when the game starts or when spawned
+	AC_Arrow();
+	virtual void BeginPlay() override;
+	virtual void NotifyActorBeginOverlap(AActor* OtherActor) override;
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
