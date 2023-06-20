2f90e0bdac1125b9977f9c64d1e614a099eb3bd7
obdo <81908835+fortress43-dev@users.noreply.github.com>
2023-06-20 19:26:04 +0900

    Update D_OnGameWidget.h

--- Source/BlackBin/Public/D_OnGameWidget.h
+++ Source/BlackBin/Public/D_OnGameWidget.h
@@ -13,5 +13,12 @@ UCLASS()
 class BLACKBIN_API UD_OnGameWidget : public UUserWidget
 {
 	GENERATED_BODY()
+<<<<<<< HEAD
+=======
+
+public:
+	//virtual void NativeConstruct() override;
+
+
 >>>>>>> DKW_alpha_fromMain
 };
